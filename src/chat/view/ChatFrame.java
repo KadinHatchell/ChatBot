660807dd3c82676ac856494e42d1b0fbd80b6380
Kadin Hatchell <kadinahatchell@gmail.com>
2016-11-03 08:58:11 -0600

    added variables to complete test (not sure if done correctly)

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,5 +6,25 @@ import java.awt.Dimension;
 
 public class ChatFrame extends JFrame
 {
+	private ChatController baseController;
+	private ChatPanel basePanel;
+	
+	public ChatFrame(ChatController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		basePanel = new ChatPanel(baseController);
+		
+		setupFrame();
+		
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setTitle("GUI");
+		this.setSize(new Dimension(500, 500));
+		this.setVisible(true);
+	}
 
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -11,5 +11,35 @@ import java.awt.event.ActionEvent;
 public class ChatPanel extends JPanel
 {
 	private ChatController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
+	private SpringLayout baseLayout;
+	
+	public ChatPanel(ChatController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		
+		colorButton = new JButton("Color");
+		randomButton = new JButton("random");
+		baseLayout = new SpringLayout();
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
 
 }
